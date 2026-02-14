ba9dceec3e09fb561c84e79013e7200fa05dc78f
wycfirezdog <karnamsharath3@gmail.com>
2026-02-14 23:31:57 +0530

    Initial commit with project setup and basic structure established.

---
+++ WTFisThis_backend/src/main/java/com/example/demo/WtFisThisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtFisThisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtFisThisBackendApplication.class, args);
+	}
+
+}
---
+++ WTFisThis_frontend/my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-red-500 w-full h-full">
+    <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
